51316db95c0518822a5a52b188e2e8f2199bf7dd
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2020-10-08 16:07:33 -0300

    cargar y vaciar
    cargar y vaciar botella

--- TP1.cpp
+++ TP1.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 class botella{
-
 private:
 float capacidad;
 float ocupacion;
@@ -15,7 +14,10 @@ bool tapada;
 public:
 botella(float );
 botella(float , bool , float );
-
+float getcapacidad();
+void llenar(float aumentar);
+float getocupacion();
+void vaciar(float disminuir);
 };
 
 botella::botella(float capacidad_total){
@@ -26,10 +28,38 @@ capacidad = capacidad_total;
 tapada = tapa;
 ocupacion = ocu;
 }
+float botella::getcapacidad(){
+return capacidad;
+}
+void botella::llenar(float aumentar){
+if(ocupacion+aumentar <= capacidad){
+    ocupacion += aumentar;
+}
+else{ocupacion = capacidad;}
+
+}
+float botella::getocupacion(){
+return ocupacion;
+}
+void botella::vaciar(float disminuir){
+if(ocupacion-disminuir >= 0){
+    ocupacion -= disminuir;
+}
+else{ocupacion = 0;}
+
+}
 
 
 int main(){
-botella reg(600, true, 200);
+float capacidad = 600, ocupacion = 200;
+botella reg(capacidad, true, ocupacion);
+cout<<"Capacidad: "<< reg.getcapacidad();
+cout<<"Ocupacion: "<< reg.getocupacion();
+float cargar;
+cout<<endl<<endl<<"Ingrese cuanto quiere vaciar en la botella: ";
+cin>>cargar;
+reg.vaciar(cargar);
+cout<<endl<<endl<<endl<<"Ocupacion: "<< reg.getocupacion();
 
 
 return 0;}
