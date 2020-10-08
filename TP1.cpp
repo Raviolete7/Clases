c454e7a025756284f952e2523e80096d4df093ad
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2020-10-08 15:36:37 -0300

    probando
    va

--- TP1.cpp
+++ TP1.cpp
@@ -5,8 +5,31 @@
 
 using namespace std;
 
-int main(){
+class botella{
 
+private:
+float capacidad;
+float ocupacion;
+bool tapada;
+
+public:
+botella(float );
+botella(float , bool , float );
+
+};
+
+botella::botella(float capacidad_total){
+capacidad = capacidad_total;
+}
+botella::botella(float capacidad_total, bool tapa, float ocu){
+capacidad = capacidad_total;
+tapada = tapa;
+ocupacion = ocu;
+}
+
+
+int main(){
+botella reg(600, true, 200);
 
 
 return 0;}
